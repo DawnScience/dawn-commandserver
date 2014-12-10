c8ad1329c5040c77a38745193f28b134e4f0c57b
Michael Wharmby <michael.wharmby@diamond.ac.uk>
2014-12-10 17:36:31 +0000

    Corrected typo in var search

--- org.dawnsci.commandserver.jython/src/org/dawnsci/commandserver/jython/python_funcs.py
+++ org.dawnsci.commandserver.jython/src/org/dawnsci/commandserver/jython/python_funcs.py
@@ -3,7 +3,8 @@ Shared py/jython functions which for use in consumer interpreter instances
 '''
 
 #Borrowed from http://blog.datasingularity.com/?p=134
+#N.b. if we add methods starting '_' they will not be nuked.
 def __dlsCleanup():
 	"""Clear's all non-standard global values from namespace"""
-	for uniquevar in [var for var in globals().copy() if var[0] != "_" and var != 'dlsCleanup']:
+	for uniquevar in [var for var in globals().copy() if var[0] != "_" and var != '__dlsCleanup']:
 		del globals()[uniquevar]

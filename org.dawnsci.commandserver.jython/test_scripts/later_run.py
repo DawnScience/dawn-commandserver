68141aaa5ca8ef70ac5506c13e86eb567fe0156a
Michael Wharmby <michael.wharmby@diamond.ac.uk>
2014-12-10 17:27:38 +0000

    Test scripts to check jython environment of consumer.

---
+++ org.dawnsci.commandserver.jython/test_scripts/later_run.py
@@ -0,0 +1,2 @@
+print "This is run 2"
+print 'dir(PdrCalInf): '+pdrCalInf_dir
--- org.dawnsci.commandserver.jython/test_scripts/report_paths.py
+++ org.dawnsci.commandserver.jython/test_scripts/report_paths.py
@@ -6,26 +6,41 @@ import sys
 
 print "\n\n*************************************************************"
 
-print sys.executable
+print 'Executable: '+str(sys.executable)
+
+print sys.path
+
+term = 'dawb'
+print 'Searching for '+term+'...'
 for p in sys.path:
-	if "diffraction" in p:
+	if term in p:
 		print p
+		
+print "\n \n"
 
 import java.lang as jl
 print "Java classpath: \n"+jl.System.getProperty("java.class.path")
+for p in jl.System.getProperty("java.class.path"):
+	if "diffraction" in p:
+		print p
+
+print "cachedir: "+jl.System.getProperty("python.cachedir")
 
 import scisoftpy as dnp
 # #For powder calibration and integration
 from uk.ac.diamond.scisoft.diffraction.powder import PowderCalibration as Cal
-# from uk.ac.diamond.scisoft.diffraction.powder import PowderCalibrationInfoImpl as PdrCalInf
-# from uk.ac.diamond.scisoft.diffraction.powder import SimpleCalibrationParameterModel# as SimpleCalPrmModel
-# from uk.ac.diamond.scisoft.analysis.crystallography import CalibrationFactory as Fac
-# from uk.ac.diamond.scisoft.analysis.diffraction.powder import PixelSplittingIntegration as PSI
-# #For outputting a NeXus file of the calibration result
-# from org.dawb.common.services import IPersistenceService
-# from org.dawb.common.services import IPersistentFile
-# from org.dawnsci.persistence import PersistenceServiceCreator
-# from com.fasterxml.jackson.core import JsonProcessingException
+from uk.ac.diamond.scisoft.diffraction.powder import PowderCalibrationInfoImpl as PdrCalInf
+from uk.ac.diamond.scisoft.diffraction.powder import SimpleCalibrationParameterModel# as SimpleCalPrmModel
+from uk.ac.diamond.scisoft.analysis.crystallography import CalibrationFactory as Fac
+from uk.ac.diamond.scisoft.analysis.diffraction.powder import PixelSplittingIntegration as PSI
+#For outputting a NeXus file of the calibration result
+from org.eclipse.dawnsci.analysis.api.persistence import IPersistenceService
+from org.eclipse.dawnsci.analysis.api.persistence import IPersistentFile
+from org.dawnsci.persistence import PersistenceServiceCreator
+from com.fasterxml.jackson.core import JsonProcessingException
 
 
 print "And now, Java classpath: \n"+jl.System.getProperty("java.class.path")
+
+pdrCalInf_dir = str(dir(PdrCalInf))
+print 'dir(PdrCalInf): '+pdrCalInf_dir

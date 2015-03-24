f3d779aa4105f35b2f425ec423ce26876548a8b5
Matthew Gerring <matthew.gerring@diamond.ac.uk>
2015-03-24 16:03:20 +0000

    Moved checker that consumer is still alive to special class.

--- org.dawnsci.commandserver.test/src/org/dawnsci/commandserver/test/Xia2ConsumerTest.java
+++ org.dawnsci.commandserver.test/src/org/dawnsci/commandserver/test/Xia2ConsumerTest.java
@@ -14,6 +14,7 @@ import javax.jms.Topic;
 import org.dawnsci.commandserver.core.ConnectionFactoryFacade;
 import org.dawnsci.commandserver.core.consumer.Constants;
 import org.dawnsci.commandserver.core.consumer.ConsumerBean;
+import org.dawnsci.commandserver.core.consumer.HeartbeatChecker;
 import org.dawnsci.commandserver.mx.consumer.MXSubmissionConsumer;
 import org.junit.Test;
 
@@ -29,7 +30,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
  */
 public class Xia2ConsumerTest {
 
-	private volatile boolean ok = false;
+
 	/**
 	 * This test fails if the Xia2Consumer cannot be located
 	 * in activemq.
@@ -41,54 +42,8 @@ public class Xia2ConsumerTest {
 	@Test
 	public void testXia2BeingConsumed() throws Exception {
 		
-		ConnectionFactory connectionFactory = ConnectionFactoryFacade.createConnectionFactory(new URI("tcp://sci-serv5.diamond.ac.uk:61616"));
-		Connection topicConnection = connectionFactory.createConnection();
-        try {
-        	topicConnection.start();
-
-        	Session session = topicConnection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-
-        	final Topic           topic    = session.createTopic(Constants.ALIVE_TOPIC);
-        	final MessageConsumer consumer = session.createConsumer(topic);
-
-	        final ObjectMapper mapper = new ObjectMapper();
-
-	        ok = false;
-	        
-	        final Thread runnerThread = Thread.currentThread();
-	        
-        	MessageListener listener = new MessageListener() {
-        		public void onMessage(Message message) {		            	
-        			try {
-        				if (message instanceof TextMessage) {
-        					TextMessage t = (TextMessage) message;
-        					ConsumerBean  b = mapper.readValue(t.getText(), ConsumerBean.class);
-        					if (!MXSubmissionConsumer.NAME.equals(b.getName())) {
-        						ok = false;
-                				System.out.println("The consumer name is incorrect!");
-                				Thread.dumpStack();
-                				runnerThread.interrupt();
-        					}
-        					System.out.println(b.getName()+ " is alive and well, thankyou.");
-        					ok = true;
-        				}
-        			} catch (Exception e) {
-        				ok = false;
-        				e.printStackTrace();
-        				runnerThread.interrupt(); // Stop it sleeping...
-        			}
-        		}
-        	};
-        	consumer.setMessageListener(listener);
-
-            Thread.sleep(10000);
-            
-            if (!ok) throw new Exception("Xia2 Consumer Heartbeat not encountered!");
-        	
-			System.out.println("The patient's heartbeat is regular and they are alive, thank heavens for stethoscopes!");
-
-        } finally {
-            topicConnection.close();
-        }
+		HeartbeatChecker checker = new HeartbeatChecker(new URI("tcp://sci-serv5.diamond.ac.uk:61616"), MXSubmissionConsumer.NAME, 10000);
+		checker.checkPulse();
+		System.out.println("The patient "+MXSubmissionConsumer.NAME+" is alive and well Dr.");
 	}
 }
